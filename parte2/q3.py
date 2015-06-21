1ecad0dd5613cfaf1441d40b5cb5dcbaefe177d4
Hélio Meira Lins <heliowml@gmail.com>
2015-06-21 14:10:44 -0300

    part 2 question 3.

---
+++ parte2/q3.py
@@ -0,0 +1,10 @@
+import numpy as np
+from scipy.misc import imsave, imread
+
+dalton = imread('dalton.bmp')
+
+cb = np.copy(dalton)
+cb[:, :, 0] = 255 / 2 + (dalton[:, :, 0] - dalton[:, :, 1])
+cb[:, :, 1] = 255 / 2 + (dalton[:, :, 1] - dalton[:, :, 0])
+
+imsave('color_blind.bmp', cb)
